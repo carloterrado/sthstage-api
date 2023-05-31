66c6a48a45a0c4fe8cddc2ea998e2efe2be857eb
carloterrado <iamterradocarlo@gmail.com>
2023-05-31 15:45:02 -0700

    add make and model by vehicle

--- app/Http/Controllers/V1/CatalogController.php
+++ app/Http/Controllers/V1/CatalogController.php
@@ -9,11 +9,12 @@ use App\Http\Resources\V1\CatalogVendorLocationResource;
 use App\Http\Resources\V1\CatalogWheelResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class CatalogController extends Controller
 {
-   
+
     public function getWheels(Request $request)
     {
         if ((!$request->has('wheel_diameter') && !$request->has('wheel_width')) && ($request->has('brand') || $request->has('mspn'))) {
@@ -54,21 +55,21 @@ class CatalogController extends Controller
 
     public function getTires(Request $request)
     {
-        if((!$request->has('section_width') && !$request->has('aspect_ratio') && !$request->has('rim_diameter')) && ($request->has('brand') || $request->has('mspn'))){
+        if ((!$request->has('section_width') && !$request->has('aspect_ratio') && !$request->has('rim_diameter')) && ($request->has('brand') || $request->has('mspn'))) {
             $data = DB::connection('tire_connect_api')
-            ->table('catalog')
-            ->where(['category' => 1])
-            ->when($request->has('brand'), function ($query) use ($request) {
-                $query->where('brand', $request->brand);
-            })
-            ->when($request->has('mspn'), function ($query) use ($request) {
-                $query->where('mspn', $request->mspn);
-            })
-            ->get();
+                ->table('catalog')
+                ->where(['category' => 1])
+                ->when($request->has('brand'), function ($query) use ($request) {
+                    $query->where('brand', $request->brand);
+                })
+                ->when($request->has('mspn'), function ($query) use ($request) {
+                    $query->where('mspn', $request->mspn);
+                })
+                ->get();
             return CatalogTireResource::collection($data);
         }
 
-        if($request->has('section_width') && $request->has('aspect_ratio') && $request->has('rim_diameter')  ){
+        if ($request->has('section_width') && $request->has('aspect_ratio') && $request->has('rim_diameter')) {
             $data = DB::connection('tire_connect_api')->table('catalog')
                 ->where([
                     'section_width' => $request->section_width,
@@ -85,43 +86,42 @@ class CatalogController extends Controller
             return CatalogTireResource::collection($data);
         }
 
-      
-            return response()->json([
-                'error' => 'Missing Parameter',
+
+        return response()->json([
+            'error' => 'Missing Parameter',
             'message' => 'The required parameters for tires are missing in the request.'
-            ], 400);
-        
+        ], 400);
     }
 
-
-
-    public function getLocation(Request $request){
+    public function getLocation(Request $request)
+    {
         //kuha partnumber sa inventory feed
-        if($request->has('part_number')) {
+        if ($request->has('part_number')) {
             $data = DB::connection('tire_connect_api')
-            ->table('inventory_feed AS i')
-            ->select('v.id', 
-            'v.short_code', 
-            'v.name', 
-            'v.email', 
-            'v.vast_vendor_number', 
-            'i.store_location_id',
-            's.addr', 
-            's.city', 
-            's.state',
-            's.zip_code',
-            's.lat',
-            's.lon',
-            's.phone',
-            's.cut_off'
-            )
-            ->join('vendor_main AS v', 'v.id', '=', 'i.vendor_main_id')
-            ->join('store_location AS s', 's.id', '=', 'i.store_location_id')
-            ->where('i.part_number', '=', $request->get('part_number'))
-            ->get();
+                ->table('inventory_feed AS i')
+                ->select(
+                    'v.id',
+                    'v.short_code',
+                    'v.name',
+                    'v.email',
+                    'v.vast_vendor_number',
+                    'i.store_location_id',
+                    's.addr',
+                    's.city',
+                    's.state',
+                    's.zip_code',
+                    's.lat',
+                    's.lon',
+                    's.phone',
+                    's.cut_off'
+                )
+                ->join('vendor_main AS v', 'v.id', '=', 'i.vendor_main_id')
+                ->join('store_location AS s', 's.id', '=', 'i.store_location_id')
+                ->where('i.part_number', '=', $request->get('part_number'))
+                ->get();
 
             return CatalogVendorLocationResource::collection($data);
-        }else {
+        } else {
             return response()->json([
                 'error' => 'Missing Parameter',
                 'message' => 'At least one parameter of brand or mspn is required.'
@@ -134,5 +134,54 @@ class CatalogController extends Controller
 
 
         //hanapin yung vendor main id sa store location
+    }
+    public function getVehicleByMakes(Request $request)
+    {
+      
+        // $token = "bdd7a30c-7c2e-4982-a236-fa37e0e6dede";
+
+        $requestYear = [
+            'Year' => $request->year
+
+        ];
+
+        $response = Http::withHeaders(['Content-Type' => 'application/json'])
+            ->post("https://api.ridestyler.net/Vehicle/GetMakes?Token=" . $request->token, $requestYear);
+
+        return $responseData = $response->json();
+
+        $makeNames = array_map(function ($make) {
+            return ['VehicleMakeName' => $make['VehicleMakeName']];
+        }, $responseData['Makes']);
+        
+        return response()->json(['Makes' => $makeNames]);
+
+      
+    }
+
+
+    public function getVehicleByModels(Request $request)
+    {
+      
+        // $token = "bdd7a30c-7c2e-4982-a236-fa37e0e6dede";
+
+        $requestYear = [
+            'Year' => $request->year,
+            'VehicleMake' => $request->makes
+
+        ];
+
+        $response = Http::withHeaders(['Content-Type' => 'application/json'])
+            ->post("https://api.ridestyler.net/Vehicle/GetModels?Token=" . $request->token, $requestYear);
+
+        return $responseData = $response->json();
+
+        $makeNames = array_map(function ($make) {
+            return ['VehicleMakeName' => $make['VehicleMakeName']];
+        }, $responseData['Makes']);
+        
+        return response()->json(['Makes' => $makeNames]);
+
+      
     }
 }
