6e29907114135a915a0cca3f10c29fc8a4430d17
vloading <vloading0@gmail.com>
2023-06-07 11:20:50 -0700

    1

--- resources/views/settings/users/users.blade.php
+++ resources/views/settings/users/users.blade.php
@@ -1,6 +1,53 @@
 @extends('layouts.mainlayout')
 
 @section('content')
+    <div class="container">
+        <div class="sticky-top">
+            <h3>Users</h3>
+            <div class="row px-3">
+                <div class="col">
+                    <b>#</b> 
+                </div>
+                <div class="col">
+                    <b>Name</b> 
+                </div>
+                <div class="col">
+                    <b>Email</b> 
+                </div>
+                <div class="col">
+                    <b>Status</b> 
+                </div>
+                <div class="col">
+                   <b>Action</b> 
+                </div>
+            </div>
+        </div>
+
+        @foreach ($users as $user)
+        <div class="row text-bg-light p-3 my-2 rounded-3">
+            <div class="col">
+                {{ $user->id }}
+            </div>
+            <div class="col">
+                {{ $user->firstname . ' ' . $user->lastname }}
+            </div>
+            <div class="col">
+                {{ $user->email }}
+            </div>
+            <div class="col">
+                {{ $user->status }}
+            </div>
+            <div class="col">
+                <a href="{{ route('user.column.settings', ['user_id' => $user->id]) }}"><button type="button"
+                    class="btn btn-primary">Edit Access</button></a>
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+@endsection
+
+{{-- @section('content')
     <table class="table">
         @if (session('success_message'))
             <div class="alert alert-success">
@@ -31,4 +78,4 @@
             @endforeach
         </tbody>
     </table>
-@endsection
+@endsection --}}
