e9276a71431d9f0f4c0346541aa880306a11a8b0
vloading <vloading0@gmail.com>
2023-06-15 10:25:00 -0700

    User front

--- resources/views/settings/settings.blade.php
+++ resources/views/settings/settings.blade.php
@@ -1,10 +1,8 @@
-<nav class="nav flex-column position-fixed justify-content-center" style="height: 60vh;">
-    <div class="mx-4">
+<nav class="nav flex-column position-fixed">
+    <div class="mx-4 mt-lg-5">
         <h4>Settings</h4>
         <h6 class="my-5"><a href="{{ route('users') }}">User Management Settings</a></h6>
         <h6 class="my-5"><a href="http://127.0.0.1:8000/settings">Update Vendor Settings</a></h6> 
         <h6 class="my-5"><a href="#">Catalog Settings</a></h6> 
     </div>
 </nav>
-
-
--- resources/views/settings/users/users.blade.php
+++ resources/views/settings/users/users.blade.php
@@ -1,113 +1,53 @@
 @extends('layouts.mainlayout')
 
-{{-- @section('content')
-    <div class="container">
-        <h3>Users</h3>
-        <table class="table table-borderless">
-            @if (session('success_message'))
-                <div class="alert alert-success">
-                    {{ session('success_message') }}
-                </div>
-            @endif
-            <thead class="sticky-top">
-                <tr class="">
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                @foreach ($users as $user)
-                <div class="my-2">
-                    <tr class="bg-white">
-                        <th scope="row">{{ $user->id }}</th>
-                        <td>{{ $user->firstname . ' ' . $user->lastname }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ $user->status }}</td>
-                        <td><a href="{{ route('user.column.settings', ['user_id' => $user->id]) }}"><button type="button"
-                                    class="btn btn-primary">Edit Access</button></a></td>
-                    </tr>
-                </div>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-@endsection --}}
-
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-2 bg-secondary">
+            <!-- Sidebar -->
+            <div class="col-2 bg-dark text-light side-bar">
+                <!-- Sidebar content goes here -->
                 @include('settings.settings')
             </div>
 
+            <!-- Main Content -->
             <div class="col-10">
-                <div class="container my-3">
-                    <h3>Users</h3>
-                    <div class="row sticky-top bg-light px-3 py-2 align-items-center">
-                        <div class="col-1">
-                            <b>
-                                <h6>#</h6>
-                            </b>
+                <div class="container">
+                    <h3 class="mt-3">User Management</h3>
+                    <div class="row">
+                        <div class="col-8">
+                            <input type="text" class="form-control" placeholder="Search">
                         </div>
                         <div class="col-4">
-                            <b>
-                                <h6>Name</h6>
-                            </b>
-                        </div>
-                        <div class="col-4">
-                            <b>
-                                <h6>Email</h6>
-                            </b>
-                        </div>
-                        <div class="col-1">
-                            <b>
-                                <h6>Status</h6>
-                            </b>
-                        </div>
-                        <div class="col-2">
-                            <b>
-                                <h6>Action</h6>
-                            </b>
+                            <div class="d-flex justify-content-end">
+                                <a href="" class="btn btn-dark me-3">FILTER</a>
+                                <a href="" class="btn btn-dark">ADD</a>
+                            </div>
                         </div>
                     </div>
 
+                    {{-- User List --}}
                     @foreach ($users as $user)
-                        <div class="row bg-white p-3 my-2 rounded-2 align-items-center">
-                            <div class="col-1">
-                                {{ $user->id }}
-                            </div>
-                            <div class="col-4">
-                                {{ $user->firstname . ' ' . $user->lastname }}
-                            </div>
-                            <div class="col-4">
-                                {{ $user->email }}
-                            </div>
-                            <div class="col-1">
-                                {{ $user->status }}
-                            </div>
-                            <div class="col-2 text-center">
-                                {{-- <a href="{{ route('user.column.settings', ['user_id' => $user->id]) }}"><button type="button"
-                            class="btn btn-secondary form-control">Edit Access</button></a> --}}
-                                <button type="button" class="btn btn-secondary form-control" data-bs-toggle="modal"
-                                    data-bs-target="#myModal-{{ $user->id }}">
-                                    Edit Access
-                                </button>
-                                @include('settings.users.user-modal')
-                            </div>
+                    <div class="row bg-white py-3 my-2 rounded-2 align-items-center">
+                        <div class="col">
+                            {{ $user->name }}
                         </div>
-                       
+                        <div class="col text-center">
+                            <a href="{{ route('user.column.settings', ['id' => $user->id]) }}"><button type="button"
+                                    class="btn btn-primary">Edit Access</button></a>
+                        </div>
+                    </div>
                     @endforeach
-
+        
                 </div>
             </div>
-@section('content')
+        </div>
+    </div>
+@endsection
+
+{{-- @section('content')
     <div class="container ">
         <div class="intro d-flex align-items-center justify-content-center">
-            <table class="table table-bordered">
+            <table class="table table-borderless">
                 @if (session('success_message'))
                     <div class="alert alert-success">
                         {{ session('success_message') }}
@@ -131,4 +71,4 @@
             </table>
         </div>
     </div>
-@endsection
+@endsection --}}
