f9305ec642a2be83447b70ce68c9b8422a9f5140
chicky-krukie <erica.flores0307@gmail.com>
2023-06-16 15:40:28 -0700

    change logout

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -15,10 +15,6 @@
 
     @yield('content')
 
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit" class="btn btn-secondary">Logout</button>
-    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     
--- routes/web.php
+++ routes/web.php
@@ -41,10 +41,3 @@ Route::middleware('auth')->group(function () {
     Route::get('/catalog', [ExcelImporterController::class, 'index'])->name('catalog');
     Route::post('/import', [ExcelImporterController::class, 'import'])->name('import');
 });
-// Route::get('/settings', [SettingsController::class, 'showSettings']);
-// Route::get('/users', [UserController::class, 'getUsers'])->name('users');
-// Route::get('/users-column-settings/{id}',[UserController::class, 'showUserCatalogSettings'])->name('user.column.settings');
-// Route::post('update-user-column-settings/{id}', [UserController::class, 'updateUserColumnSettings'])->name('update.user.column.settings');
-// Route::post('settings/catalog', [SettingsController::class, 'submitCatalog'])->name('submitCatalog');
-
-
