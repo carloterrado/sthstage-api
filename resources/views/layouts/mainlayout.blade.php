8bd393b6ef7fd95e806691cbccf0c4aa6b26de86
carloterrado <iamterradocarlo@gmail.com>
2023-06-16 15:40:22 -0700

    merge documentation

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -23,7 +23,7 @@
     </form>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+ 
     
 </body>
 
